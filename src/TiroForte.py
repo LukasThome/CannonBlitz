28b25bcebbf0acd4ec3b00f7b4a42ec79ba00995
Luiz Fernando Schelbauer <nandoschelba@gmail.com>
2024-07-06 11:39:36 -0300

    Tiro forte

--- src/TiroForte.py
+++ src/TiroForte.py
@@ -1,19 +1,25 @@
-# #!/usr/bin/python
-# # -*- coding: UTF-8 -*-
-# from Dominio_do_problema import Canhao
-# from Dominio_do_problema import Tiro
-
-# class TiroForte(Tiro):
-# 	def __init__(self):
-# 		self._unnamed_Canhao_ = None
-# 		"""@AttributeType Dominio do problema.Canhao
-# 		# @AssociationType Dominio do problema.Canhao"""
-
-# Importa a classe Tiro corretamente
-from Tiro import Tiro
+import random
+from Tiro import Tiro  # Certifique-se de importar corretamente a classe Tiro
 
 class TiroForte(Tiro):
     def __init__(self):
         super().__init__()
-        # Inicialização adicional para TiroForte
-        pass
+        self.area_acerto = 9
+
+    def atirar(self, posicoes_campo):
+        todas_as_posicoes = [(x, y) for x in range(3) for y in range(5)]
+        campo = [pos for pos in todas_as_posicoes if pos not in posicoes_campo]
+
+        # Escolhe uma posição central para o tiro forte
+        posicao_central = random.choice(campo)
+
+        # Define a área 3x3 ao redor da posição central
+        posicoes_atingidas = []
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                pos = (posicao_central[0] + dx, posicao_central[1] + dy)
+                if pos in todas_as_posicoes and pos not in posicoes_atingidas:
+                    posicoes_atingidas.append(pos)
+
+        self.area_acerto = len(posicoes_atingidas)
+        return posicoes_atingidas
